5dbc666d81883dc6cfce0973a8e1b9d63c72c1ae
Venkat Shiva Reddy <venkatshiva.reddy@gmail.com>
2026-02-05 13:04:03 +0530

    mvc 2

---
+++ ProductCatelogMVCWebApp/Controllers/ProductsController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ProductCatelogMVCWebApp.Models.DataAccess;
+using ProductCatelogMVCWebApp.Models.Entities;
+
+namespace ProductCatelogMVCWebApp.Controllers
+{
+    public class ProductsController : Controller
+    {
+        ProductsDbContext db = new ProductsDbContext();
+
+        public IActionResult Index()
+        {
+            // fetch the data from model
+            List<Product> products = db.Products.ToList();
+            // pass the data to view
+
+
+            return View(products);
+        }
+    }
+}
