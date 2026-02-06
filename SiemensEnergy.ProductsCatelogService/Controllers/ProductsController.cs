9aea6d7e35d2456f6ec42c187c987937a0324738
Venkat Shiva Reddy <venkatshiva.reddy@gmail.com>
2026-02-06 13:07:14 +0530

    api 3

--- SiemensEnergy.ProductsCatelogService/Controllers/ProductsController.cs
+++ SiemensEnergy.ProductsCatelogService/Controllers/ProductsController.cs
@@ -6,18 +6,36 @@ namespace SiemensEnergy.ProductsCatelogService.Controllers
     [ApiController]
     public class ProductsController : ControllerBase
     {
-
+        SiemensProductCatelogMvcwebAppDbContext db = new SiemensProductCatelogMvcwebAppDbContext();
 
         // GET http://domain.com/api/Products
+        [HttpGet]
         public List<Product> GetAllProducts()
         {
             // fetch products from dal
-            SiemensProductCatelogMvcwebAppDbContext db = new SiemensProductCatelogMvcwebAppDbContext();
+
 
             var products = db.Products.ToList();
             return products;
         }
 
+
+        // .../api/products/123
+        [Route("{id}")]
+        [HttpGet]
+        public IActionResult GetProductById(int id)
+        {
+            var product = db.Products.Find(id);
+            if (product != null)
+                return Ok(product); // 200 and data
+            else
+            {
+                // 404
+                return NotFound();
+            }
+
+
+        }
 
     }
 }
