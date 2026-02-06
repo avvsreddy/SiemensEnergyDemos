26f5aeef86393f9b328c5223fdba7f2f3c0c2b5f
Venkat Shiva Reddy <venkatshiva.reddy@gmail.com>
2026-02-06 16:09:17 +0530

    api

--- SiemensEnergy.ProductsCatelogService/Controllers/ProductsController.cs
+++ SiemensEnergy.ProductsCatelogService/Controllers/ProductsController.cs
@@ -21,7 +21,7 @@ namespace SiemensEnergy.ProductsCatelogService.Controllers
 
 
         // .../api/products/123
-        [Route("{id}")]
+        [Route("{id:int}")]
         [HttpGet]
         public IActionResult GetProductById(int id)
         {
@@ -37,5 +37,146 @@ namespace SiemensEnergy.ProductsCatelogService.Controllers
 
         }
 
+
+        //Step 1:  Design API URI: http://domain.com/api/products/available
+        // Step 2: Map the URI to a method in controller
+        // Step 3: Map the HTTP verb to the method
+        // Step 4: Implement the method to fetch data from dal and return to client
+        [Route("available")]
+        [HttpGet]
+        public IActionResult GetAllAvailableProducts()
+        {
+            //var products = db.Products.Where(p => p.IsAvailable).ToList();
+            //var products = db.Products.Where(ProductExists).ToList();
+            //var products = db.Products.Where( delegate (Product p)
+            //    {
+            //    return p.IsAvailable;
+            //}).ToList();
+
+            var products = db.Products.Where(p => p.IsAvailable).ToList();
+
+            if (products.Count == 0)
+            {
+                return NotFound("No products are available in the stock");
+            }
+
+            return Ok(products);
+        }
+
+        //private bool ProductExists(Product p)
+        //{
+        //    return p.IsAvailable;
+        //}
+
+
+        // URI: http://domain.com/api/products/costliest
+        [HttpGet("costliest")]
+        public IActionResult GetCostliestProduct()
+        {
+            var product = db.Products.OrderByDescending(p => p.Price).FirstOrDefault();
+            if (product != null)
+                return Ok(product);
+            else
+                return NotFound("No products found");
+        }
+
+        // URI: http://domain.com/api/products/cheapest
+        [HttpGet("cheapest")]
+        public IActionResult GetCheapestProduct()
+        {
+            var product = db.Products.OrderBy(p => p.Price).FirstOrDefault();
+            if (product != null)
+                return Ok(product);
+            else
+                return NotFound("No products found");
+
+        }
+
+
+        // get all products in a category
+        // URI: http://domain.com/api/products/category/electronics
+        [HttpGet("category/{category:alpha}")]
+        public IActionResult GetProductsByCategory(string category)
+        {
+            var products = db.Products.Where(p => p.Category.Contains(category)).ToList();
+            if (products.Count > 0)
+                return Ok(products);
+            else
+                return NotFound($"No products found in category: {category}");
+        }
+
+        // get product by name
+        // URI: http://domain.com/api/products/name/i phone 14
+        [HttpGet("name/{name:alpha}")]
+        public IActionResult GetProductByName(string name)
+        {
+            var product = db.Products.FirstOrDefault(p => p.ProductName.Contains(name));
+            if (product != null)
+                return Ok(product);
+            else
+                return NotFound($"No product found with name: {name}");
+
+
+        }
+
+        // get products in a price range
+        // URI: http://domain.com/api/products/price/min/{price}/max/{price}
+        [HttpGet("price/min/{minPrice:int}/max/{maxPrice:int}")]
+        public IActionResult GetProductsByPriceRange(int minPrice, int maxPrice)
+        {
+            var products = db.Products.Where(p => p.Price >= minPrice && p.Price <= maxPrice).ToList();
+            if (products.Count > 0)
+                return Ok(products);
+            else
+                return NotFound($"No products found in price range: {minPrice} - {maxPrice}");
+        }
+
+
+        // Delete a product by id
+        // URI: DELETE http://domain.com/api/products/123
+        [HttpDelete("{id:int}")]
+        public IActionResult Delete(int id)
+        {
+            var product = db.Products.Find(id);
+            if (product != null)
+            {
+                db.Products.Remove(product);
+                db.SaveChanges();
+                return Ok($"Product with id {id} deleted successfully");
+            }
+            else
+            {
+                return NotFound($"No product found with id: {id}");
+            }
+        }
+
+        // add new product
+        // URI: POST http://domain.com/api/products
+        [HttpPost]
+        public IActionResult AddProduct(Product product)
+        {
+            db.Products.Add(product);
+            db.SaveChanges();
+            //return Ok($"Product with id {product.ProductId} added successfully");
+
+            // return 201 and URI of newly created resource in location header
+            //return CreatedAtAction(nameof(GetProductById), new { id = product.ProductId }, product);
+
+            // 201 + location + product data
+            return Created($"api/products/{product.ProductId}", product);
+
+        }
+
+        // edit a product
+        // URI: PUT http://domain.com/api/products/
+        [HttpPut]
+        public IActionResult UpdateProduct([FromBody] Product product)
+        {
+
+            db.Products.Update(product);
+            db.SaveChanges();
+
+            return Ok($"Product with id {product.ProductId} updated successfully");
+        }
     }
 }
